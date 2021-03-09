6bb26004cdcb66af2cd9f55685215b90804c541d
Mayank Gandhi <mayankgandhi50@gmail.com>
2021-03-09 11:11:59 +0530

    Initial Commit

---
+++ YogaVision/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YogaVision
+//
+//  Created by Mayank Gandhi on 3/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
